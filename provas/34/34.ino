8b43ac1a49b93815b9ea4bf5c46a6828b5d83ad7
Yuri Komuta <33011150+yurikomuta@users.noreply.github.com>
2026-05-15 14:11:13 +0000

    add wokwi e code 34

---
+++ provas/34/34.ino
@@ -0,0 +1,18 @@
+//definição do pino de LED
+
+#define Led_Pin 26
+
+void setup() {
+  // configura o pino como saída 
+  Serial.begin(115200);
+  pinMode(Led_Pin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(Led_Pin, HIGH);
+  delay(400); // espera 
+  digitalWrite(Led_Pin, LOW);
+  delay(800); // espera 
+}
+
+
